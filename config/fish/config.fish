ff86c00267e0111c728572616f8bcbbabe2994ba
basti <bhuber@testo.de>
2023-12-06 08:13:19 +0100

    removed wm specific env vars

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,11 +6,6 @@ if status is-interactive
     fish_add_path -aP $HOME/Applications/flutter/bin
     fish_add_path -aP $HOME/.ghcup/bin
     set -gx EDITOR vim
-    export AAI_API_KEY=dd3e478664334c4cb4f0c2f5b49019eb
-    export CHROME_EXECUTABLE=google-chrome-stable
-    export QT_QPA_PLATFORM="wayland;xcb"
-    export QT_QPA_PLATFORMTHEME=qt5ct
-    export GDK_BACKEND=wayland
 
     set theme_color_scheme gruvbox
 
