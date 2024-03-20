64d3e6be7ff48ccfc0aca4ffa6050c0f85c87a9e
basti <huberbas@posteo.net>
2024-03-20 10:24:55 +0100

    restructure and add file sync script

--- config/nvChad_custom/README.md
+++
@@ -1,3 +0,0 @@
-# NvChad Custom Directory
-
-Custom part of the NvChad Cofig, to be inserted into NvChad default installation.
--- config/nvim/lua/core/plugin_config/nvim-tree.lua
+++
@@ -1,7 +0,0 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrmPlugin = 1
-
-require("nvim-tree").setup()
-
--- Toggle with Ctrl+n
-vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
