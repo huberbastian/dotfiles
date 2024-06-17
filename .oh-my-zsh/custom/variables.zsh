2aa37a04713a29d94add2025b9b9d4d177f70f58
basti <huberbas@posteo.net>
2024-06-17 10:48:17 +0200

    update conf, delete dirs, update script

--- .oh-my-zsh/custom/variables.zsh
+++ .oh-my-zsh/custom/variables.zsh
@@ -2,5 +2,6 @@ export CHROME_EXECUTABLE=google-chrome-stable
 export QT_QPA_PLATFORM="wayland;xcb"
 export QT_QPA_PLATFORMTHEME=qt5ct
 export GDK_BACKEND=wayland
+export EDITOR=nvim
 
 export AUTO_NOTIFY_WHITELIST=("pacman" "yay")
